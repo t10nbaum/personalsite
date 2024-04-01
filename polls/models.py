6d54c603295e8ecc06163d9461f78b5c4c45da37
t10nbaum <tenenbaum.jason@gmail.com>
2024-03-31 22:18:42 -0400

    First push of django tutorial

---
+++ polls/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+# Create your models here.
+
+class Question(models.Model):
+    """Model definition for Question."""
+
+    # TODO: Define fields here
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    class Meta:
+        """Meta definition for Question."""
+
+        verbose_name = 'Question'
+        verbose_name_plural = 'Questions'
+
+    def __str__(self):
+        """Unicode representation of Question."""
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    """Model definition for Choice."""
+
+    # TODO: Define fields here
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    class Meta:
+        """Meta definition for Choice."""
+
+        verbose_name = 'Choice'
+        verbose_name_plural = 'Choices'
+
+    def __str__(self):
+        """Unicode representation of Choice."""
+        return self.choice_text
